89e02db737574a1aea749ce36c332b2cee02d608
RaphaelHGarcia <raphael.henriquegarcia@gmail.com>
2018-10-17 19:08:40 -0300

    Incluindo funcionalidade de inscrição

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+// const bodyParser = require('body-parser')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
 const PORT = 3000
 
+app.use(express.json())
+app.use(express.urlencoded())
 app.use(express.static('public'))
 app.use(express.static('views'))
+
 app.get('/', (req, res) => res.render('index.html'))
+app.post('/', (req, res) => {
+  db.get('subscribers')
+    .push({ name: req.body.name, email: req.body.email })
+    .write()
+
+    res.redirect('/?=success')
+})
 
 app.listen(PORT, () => console.log(`Site running on port ${PORT}!`))
