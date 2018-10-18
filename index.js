cdb8884c23c41e9c2df5e16afb9e9256d7c0bb26
RaphaelHGarcia <raphael.henriquegarcia@gmail.com>
2018-10-17 21:28:22 -0300

    a

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
-const adapter = new FileSync('./public/db.json')
+const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 const PORT = 3000
+
+const subs = require('./db.json')
 
 app.use(express.json())
 app.use(express.urlencoded())
@@ -15,13 +17,13 @@ app.use(express.static('public'))
 app.use(express.static('views'))
 
 app.get('/', (req, res) => res.render('index.html'))
-app.get('/subs', (req, res) => res.send('db.json'))
+app.get('/subs', (req, res) => res.send(JSON.stringify(subs)))
 app.post('/', (req, res) => {
   db.get('subscribers')
     .push({ name: req.body.name, email: req.body.email })
     .write()
 
-    res.redirect('/?=success#banner')
+    res.redirect('/?=success#')
 })
 
 app.listen(PORT, () => console.log(`Site running on port ${PORT}!`))
